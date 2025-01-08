d183cce39288860935d13fb302c41da6b7a72382
Ederam <53803895+Ederam@users.noreply.github.com>
2025-01-08 14:14:56 -0500

    Fin ajustes ambiente
    Actualizacion con creacion y configuracion de base de datos en ambiente local

---
+++ TodoListApi/Scripts/PurchaseOrderDetail.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE dbo.PurchaseOrderDetail (
+    PurchaseOrderID INT NOT NULL,
+    LineNumber SMALLINT NOT NULL,
+    ProductID INT NULL,
+    UnitPrice MONEY NULL,
+    OrderQty SMALLINT NULL,
+    ReceivedQty FLOAT NULL,
+    RejectedQty FLOAT NULL,
+    DueDate DATETIME NULL
+);
